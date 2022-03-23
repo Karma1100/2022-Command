3e76a1394542cc2c4d1e463b3e69c20c37eb4203
AJ <ajdefafa@gmail.com>
2022-03-23 11:50:22 -0400

    New command To Deploy the intake

--- src/main/java/frc/robot/commands/IntakeRetrieve.java
+++ src/main/java/frc/robot/commands/IntakeRetrieve.java
@@ -5,14 +5,11 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.subsystems.DriveTrain;
 
-public class Turn extends CommandBase {
-  DriveTrain m_drive;
-  double m_degree;
-  public Turn(DriveTrain drive, double turn) {
-    m_drive = drive;
-    m_degree = turn;
+public class IntakeRetrieve extends CommandBase {
+  /** Creates a new IntakeRetrieve. */
+  public IntakeRetrieve() {
+    // Use addRequirements() here to declare subsystem dependencies.
   }
 
   // Called when the command is initially scheduled.
@@ -21,15 +18,11 @@ public class Turn extends CommandBase {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {
-    m_drive.turn(m_degree);
-  }
+  public void execute() {}
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {
-    m_drive.drive(0, 0);
-  }
+  public void end(boolean interrupted) {}
 
   // Returns true when the command should end.
   @Override
